007ef90a3f725d8e70accfd13c5d5b3785a4b31b
leechungheon <leechung6311@gmail.com>
2025-11-24 19:05:54 +0900

    refactor(main): 필드 삭제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,8 @@ import org.example.view.io.InputHandler;
 import org.example.view.io.OutputHandler;
 
 public class Main {
-    private final OutputHandler outputHandler;
-    private final InputHandler inputHandler;
+
     public Main(OutputHandler outputHandler, InputHandler  inputHandler) {
-        this.outputHandler = outputHandler;
-        this.inputHandler = inputHandler;
     }
 
     public static void main(String[] args){
